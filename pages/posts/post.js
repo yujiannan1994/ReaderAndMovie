a2139d5b5c04b5c7c3cf5e6014a7e3bb307fe9b5
yujiannan1994 <yujiannan1994@gmail.com>
2018-03-21 23:32:53 +0800

    test page life cycle

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,3 +1,25 @@
 Page({
-
+  data: {
+    text: "This is page data."
+  },
+  onLoad: function (options) {
+    // Do some initialize when page load.
+    console.log("onLoad");
+  },
+  onReady: function () {
+    // Do something when page ready.
+    console.log("onReady");
+  },
+  onShow: function () {
+    // Do something when page show.
+    console.log("onShow");
+  },
+  onHide: function () {
+    // Do something when page hide.
+    console.log("onHide");
+  },
+  onUnload: function () {
+    // Do something when page close.
+    console.log("onUnload");
+  }
 })
