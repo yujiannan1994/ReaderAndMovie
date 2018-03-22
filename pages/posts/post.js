6259d55a4bd35f2fbe9d50a3be9253d77b64b814
yujiannan1994 <yujiannan1994@gmail.com>
2018-03-22 23:27:18 +0800

    data binding

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,10 +1,22 @@
 Page({
   data: {
-    text: "This is page data."
+    text: "This is page data.",
+    date: "Mar 19 2018",
+    title: "正是虾肥蟹壮时"
   },
   onLoad: function (options) {
     // Do some initialize when page load.
     console.log("onLoad");
+    var post_content1 = {
+      date: "Mar 19 2018",
+      title: "正是虾肥蟹壮时",
+      post_img: "/images/post/米诺·路飞1366.jpg",
+      content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“鳌封嫩玉双双满”",
+      view_num: "112",
+      collect_num: "96",
+      author_img: "/images/avatar/avatar.png"
+    }
+    this.setData(post_content1);
   },
   onReady: function () {
     // Do something when page ready.
