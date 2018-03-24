c3cfab088714cb91358442bb1ad264be92347171
yujiannan1994 <yujiannan1994@gmail.com>
2018-03-24 23:28:07 +0800

    page redirect

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -33,18 +33,18 @@ Page({
   },
   onReady: function () {
     // Do something when page ready.
-    console.log("onReady");
+    
   },
   onShow: function () {
     // Do something when page show.
-    console.log("onShow");
+    
   },
   onHide: function () {
     // Do something when page hide.
-    console.log("onHide");
+    
   },
   onUnload: function () {
     // Do something when page close.
-    console.log("onUnload");
+    
   }
 })
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,19 @@
 Page({
-  
+  onTap: function() {
+    // wx.navigateTo({
+    //   url: "../posts/post"
+    // });
+
+    wx.redirectTo({
+      url: "../posts/post"
+    });
+  },
+
+  onUnload: function() {
+    // console.log("welcome page is unload");
+  },
+
+  onHide: function () {
+    // console.log("welcome page is hide");
+  }
 })
